33bb3fb7c690cc91149f9406c07140ccf11b0a83
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-09 21:47:04 -0700

    commit task zero_2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+
 /**
-* more headers goes there 
-* betty style doc for function main goes there
 * main - Entry point
 * if - evaluates condition
 * printf - prints out the true condition
 * Return: Always 0 (Success)
 */
+
 int main(void)
 
 {
