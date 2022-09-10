82be86002126c29e623ab6d225fcb36d5e61d86f
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-10 11:31:59 -0700

    task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * putchar - prints out the alphabets
+ * Return: Always 0 (Success)
+ */
+
+int main()
+
+{
+
+	        char c;
+
+		c = 'a';
+		while(c <= 'z')
+
+		{
+
+			 putchar(c);
+
+											     		 putchar('\n');
+											                c = c+1;
+												 }
+
+
+		return (0);
+
+}
+
