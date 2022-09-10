ba6d6e51f3639fe46ece6bfcf0670c8e17f6cccf
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-09 23:30:54 -0700

    task 1 commit_2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -38,7 +38,7 @@ int main(void)
 		}
 		else
 		{
-			printf("Last diit of %d is %d and is less than 6 and not 0\n", n, m);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 		}
 		return (0);
 }
