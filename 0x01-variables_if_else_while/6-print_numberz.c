f3628ebc0a3b423cf18950e592ae31036b300ab4
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-10 21:54:57 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * putchar - out put the result
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
