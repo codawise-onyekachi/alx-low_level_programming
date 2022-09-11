7130aef6745541160c60bba94a982ff2e171aac9
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-10 21:16:22 -0700

    task 5 commit_2

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>                                                                                                                                     
-/**                                                                             * main - Entry point
-* printf - prints out the numbers 
-* Return: Always 0 (Success)                                                    */
+#include <stdio.h>
+
+/**
+* main - Entry point
+* printf - prints out the numbers
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
