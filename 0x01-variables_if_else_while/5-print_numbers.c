75ae397ff2a93b6558cacb7ba7f3ee9cbae862cd
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-10 20:57:38 -0700

    task 5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>                                                                                                                                             
+
+#include <time.h>                                                                                                                                               
+
+#include <stdio.h>                                                                                                                                     
+/**                                                                             * main - Entry point
+* printf - prints out the numbers 
+* Return: Always 0 (Success)                                                    */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
+
