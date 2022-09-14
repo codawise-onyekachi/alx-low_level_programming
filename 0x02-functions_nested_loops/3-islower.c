3042ab5e331909ebc67c82229575a9d6932d8bac
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-14 11:09:22 -0700

    latest commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - check description
+ * Description: prints the 1 or 0 as required
+ * Return: return 1 for lower case letters. 0 for the rest
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+
+	}
+
+	_putchar('\n');
+
+}
