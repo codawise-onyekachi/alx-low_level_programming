dddcd0f0fef3b47eb55c3b96be5db7dbf4894203
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-15 11:23:15 -0700

    latest commit of 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,6 +10,7 @@
 
 void jack_bauer(void)
 {
+
 	int i; /* hour */
 
 	int j; /* minute */
@@ -19,16 +20,17 @@ void jack_bauer(void)
 		for (j = 0; j <= 59; j++)
 		{
 			_putchar((i / 10) + '0');
-			
+
 			_putchar((i % 10) + '0');
-			
+
 			_putchar(':');
-			
+
 			_putchar((j / 10) + '0');
 			
 			_putchar((j % 10) + '0');
-			
+
 			_putchar('\n');
+
 		}
 	}
 }
