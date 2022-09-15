d7a5d736e5f515a222583cae31324110295a94fb
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-15 15:56:03 -0700

    for task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - Adds two integers.
- * @num1: The first integer to be added.
- * @num2: The second integer to be added.
+ * @i: The first integer to be added.
+ * @j: The second integer to be added.
  * Return: The result of the addition.
  */
 
