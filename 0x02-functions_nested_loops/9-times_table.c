bec0fe73fa9795b90751858082bc3306cbdebbea
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-15 14:47:55 -0700

    task 9 commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * times_table - Check description
+ * Description: It prints 9 times table starting with 0
+ * Return: Nothing.
+ */
+
+void times_table(void)
+{
+	int i;
+
+	int j;
+
+	int prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+
+
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(',');
+
+			_putchar(' ');
+
+			prod = i * j;
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+
+			}
+
+			else
+			{
+				_putchar((prod / 10) + '0');
+
+			}
+
+			_putchar((prod % 10) + '0');
+
+		}
+		_putchar('\n');
+
+	}
+}
