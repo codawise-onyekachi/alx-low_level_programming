f40516b14f5fb82bd82c99f5b1f9b1ccd0936d42
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-15 05:53:59 -0700

    latest commit of task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  *_abs - Check Holberton
- * @r: An integre input
+ * @n: An integre input
  * Description: This function returns absolute value of a number
  * Return: Absolut value of number r
  */
