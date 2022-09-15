52aedc7532b270565fef273d52c96f892ad2891a
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-15 03:08:05 -0700

    latest commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_sign - Check description
+ * _putchar - output sign
+ * @n: An input number
+ * Description: prints the sign of the number
+ * Return: 1 if number is positive, o if number is 0, or -1 for others
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{ 
+		_putchar('+');
+
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+
+		return (-1);
+	}
+
+	else
+	{
+		_putchar('0');
+
+		return (0);
+
+	}
+
+	_putchar('\n');
+
+}
+
