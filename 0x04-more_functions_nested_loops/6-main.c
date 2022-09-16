bbf897664a5634d93cdee89c160855c59f7dbf2b
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-16 08:57:54 -0700

    task 6 commit

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return : Always 0 (success)
+ */
+
+int main (void)
+{
+	print_line(0);
+
+	print_line(2);
+
+	print_line(10);
+
+	print_line(-4);
+
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - function description
+ * Description: output a straight line in the terminal
+ */
+
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len <= n; len++)
+		{
+			putchar('_');
+		}
+	}
+	putchar('\n');
+}
