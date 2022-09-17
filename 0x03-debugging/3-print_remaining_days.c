bd598608cd8f9610b6fba60cbe730933f4fcd240
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-17 04:44:14 -0700

    task 11

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,23 +2,27 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
+ * print_remaining_days - function description
+ * Description: takes a date and prints out how many days are
+ * remaining, taking leap year into considferation
  * @month: month in number format
+ *
  * @day: day of month
+ *
  * @year: year
  * Return: void
  */
+
+
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
+	if (year % 4 == 0)
 	{
-		if (month >= 2)
+		if ((month == 2 && day < 60) || (month > 2 && day >= 60))
 		{
 			day++;
 		}
 		printf("Day of the year: %d\n", day);
-		
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
@@ -30,7 +34,6 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,12 @@
 This task 0 is to create a file named 0-main.c. This file must test that the function positive_or_negative() gives the correct output when given a case of 0.
+
+
+Task 1: Copy the main file. Comment out (don’t delete it!) the part of the code that is causing the output to go into an infinite loop.
+Don’t add or remove any lines of code, as we will be checking your line count. You are only allowed to comment out existing code.
+You do not have to compile with -Wall -Werror -Wextra -pedantic for this task.
+
+
+Task 2 is a fix on a program that prints the largest of three integers.
+
+
+Task 3 is a fix on a program that converts a date to the day of year and determines how many days are left in the year, taking leap year into consideration
