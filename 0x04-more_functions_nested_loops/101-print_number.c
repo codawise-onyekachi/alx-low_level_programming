19a20462811110dda577b20a7fb0278c78c485a5
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-17 02:46:44 -0700

    task 12 commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - this is function description
+ * Description: prints an integer
+ * @n - the integer to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		putchar('_');
+
+		num = -num;
+	}
+	if ((num / 10) > 0)
+	{
+		print_number(num / 10);
+	}
+	putchar((num % 10) + '0');
+}
