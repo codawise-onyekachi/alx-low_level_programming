741322823fe34a8ea33de5cecbc55cdf345c8c23
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-16 19:58:03 -0700

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** main - entry point
+ * Return: Always 0 (success)
+ */
+
+int main (void)
+{
+	long factor;
+
+	long num = 612852475143;
+
+	for (factor = 2;factor < num; factor++)
+	{
+		if (num / factor == 0)
+		{
+			num = num / factor;
+		}
+
+	}
+	printf("%li\n", num);
+
+	return (0);
+
+}
