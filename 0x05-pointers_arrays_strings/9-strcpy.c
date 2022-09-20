69672ce42d0f8df8a7b8db01cc15cbba9728f0c9
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-19 23:58:13 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * strcpy - function description
+ * _strcpy - function description
  * Description:copies the string pointed to by src, including the
  *  terminating null byte (\0), to the buffer pointed to by dest
  * @dest:A pointer to destination of string
