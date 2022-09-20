6d37aa558aa9a96257cf77fc9154e45828c9b31c
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-19 18:44:31 -0700

    third commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * _strlen - function description
+ * Description: returns the lenght of string
+ * Return: Always 0
+ * @s: input string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while(s[len] != '\0')
+	{
+		len++;
+
+	}
+	return (len);
+
+}
