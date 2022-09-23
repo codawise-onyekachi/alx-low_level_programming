bfc19670a46ba286875f8e88a8d547018c131c3d
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-23 05:15:04 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchar - funtion description
+ * Description: concatenates two strings
+ * @dest: input string
+ * @src: input string
+ * Return:A pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	char *temp = dest;
+
+	while (*dest)
+	{
+		dest++;
+
+	}
+	while (*src)
+	{
+		*dest++ = *src++;
+
+	}
+
+	*dest = '\0';
+
+	return (temp);
+
+}
