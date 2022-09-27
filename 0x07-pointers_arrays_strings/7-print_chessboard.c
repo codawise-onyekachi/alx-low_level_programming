8a2b243640d489ff2eab994baff601b5c56672e5
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-26 21:45:39 -0700

    seventh commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * print_chessboard - a function that prints a chessboard
+ * @a: an array input to print
+ * Return: Nothing
  */
 
 void print_chessboard(char (*a)[8])
