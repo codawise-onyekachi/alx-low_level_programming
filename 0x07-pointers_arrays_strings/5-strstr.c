f90d509a57d027657c830265084413ea6598d761
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-26 21:35:44 -0700

    sixth commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,9 +4,10 @@
 /**
  * _strstr - function description
  * Description: function that locates a substring
- * @haystach:
- * @needle:
- * Return
+ * @haystach:an input string to search in
+ * @needle:an input string to locate into string haystack
+ * Return:a pointer to the beginning of the located
+ * substring, or NULL if the substring is not found
  */
 
 char *_strstr(char *haystack, char *needle)
