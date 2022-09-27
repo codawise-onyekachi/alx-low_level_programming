4babd9d82307345a9ace83b5a38abc6dcbef7110
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-26 20:53:20 -0700

    fifth commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,9 +4,10 @@
 /**
  * _strpbrk - function description
  * Description:  searches a string for any of a set of bytes
- * @s:
- * @accept:
- * Return:
+ * @s: input string
+ * @accept: an input character with to locate into string s
+ * Return:a pointer to the byte in s that matches one of the bytes
+ * i accept, or NULL if no such byte is found
  */
 
 char *_strpbrk(char *s, char *accept)
