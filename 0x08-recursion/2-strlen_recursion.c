56dc899d88ce0c9051e12399cf3a9ce6c8878073
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-28 17:47:29 -0700

    third commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - function description
+ * Description: prints the lenght of a string
+ * @s: string input to be printed
+ * Return:length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	while (*s)
+	{
+		return (_strlen_recursion(s + 1) + 1);
+
+	}
+	return (0);
+
+}
