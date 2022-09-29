91a202f1489865ea6409ebd3493cfe13b8e326f6
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-09-29 06:41:05 -0700

    task 8 commit

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * wildcmp - function description
+ * Descriptin:compares two strings
+ * @s1:first input string
+ * @s2:second input string
+ * Return:1 if strings identical and 0 in otherwise
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+	{
+		return (1);
+
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	}
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+	{
+		return(1);
+
+	}
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+	{
+		return (0);
+
+	}
+	return (0);
+
+}
