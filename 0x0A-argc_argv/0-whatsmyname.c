24b773ea66da3d11d0abc8d11b20bb0f4c2205c5
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-10-01 05:16:36 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function description
+ * Description: prints its name,followed by a new line
+ * argc: agument count
+ * @argv: argument value, a strings that come after calling function
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+
+	}
+	return (0);
+
+}
