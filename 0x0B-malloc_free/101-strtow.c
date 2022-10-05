309e05eb13eea9ea9557fa1f1b31f8baff8fe001
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-10-04 23:51:23 -0700

    task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,73 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - A function that splits a string into words
+ * @str: An input pointer of the string to split
+ *
+ * Return: Apointer to concatened strings or NULL if it str is NULL
+ */
+
+char **strtow(char *str)
+{
+	char **array;
+	int i = 0;
+	int j;
+	int m;
+	int k = 0;
+	int len = 0;
+	int count = 0;
+
+	if (str == NULL || *str == '\0')
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		if ((str[i] != ' ' || *str != '\t') &&
+				((str[i + 1] == ' ' || str[i + 1] == '\t') || str[i + 1] == '\n'))
+		{
+			count++;
+		}
+	}
+	if (count == 0)
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(char *) * (count + 1));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0' && k < count; i++)
+	{
+		if (str[i] != ' ' || str[i] != '\t')
+		{
+			len = 0;
+			j = i;
+
+			while ((str[j] != ' ' || str[j] != '\t') && str[j] != '\0')
+			{
+				j++;
+				len++;
+			}
+			array[k] = malloc((len + 1) * sizeof(char));
+			if (array[k] == NULL)
+			{
+				for (k = k - 1; k >= 0; k++)
+				{
+					free(array[k]);
+				}
+				free(array);
+				return (NULL);
+			}
+			for (m = 0; m < len; m++, i++)
+			{
+				array[k][m] = str[i];
+			}
+			array[k++][m] = '\0';
+		}
+	}
+	array[k] = NULL;
+	return (array);
+}
