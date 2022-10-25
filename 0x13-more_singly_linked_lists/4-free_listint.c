5d83f377a3636407f0f1ed84894e1283e24fba49
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2022-10-24 21:47:32 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint - function description
+ * Description: A function that frees a linked list
+ * @head: A pointer to listint_t structure
+ *
+ * Return: returns nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free (temp);
+
+	}
+	free(head);
+
+}
